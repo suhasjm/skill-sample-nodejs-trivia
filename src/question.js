eee52ee1f6163cd7ec2cd674132addb8c90b8de7
Suhas Manangi <suhasjm@gmail.com>
2017-09-09 12:39:17 -0700

    Update question.js

--- src/question.js
+++ src/question.js
@@ -8,324 +8,324 @@ module.exports = {
     QUESTIONS_EN_GB: [
   {
     'What food is Ross allergic to?': [
-      'Lobster',
-      'Chcolate',
-      'Milk',
-      'Ice cream'
+      'Lobster.',
+      'Chcolate.',
+      'Milk.',
+      'Ice cream.'
     ]
   },
     {
     'What according to Phoebe, makes Chandler look homosexual?': [
-      'His Hair',
-      'His Voice',
-      'His Stature',
-      'His Looks'
+      'His Hair.',
+      'His Voice.',
+      'His Stature.',
+      'His Looks.'
     ]
   },
     {
-    'Joey\'s favourite dialogue': [
-      'How you doin',
-      'I love pizza',
-      'Joey doesn\'t share food',
-      'I love sandwich'
+    'Joey\'s favourite dialogue?': [
+      'How you doin.',
+      'I love pizza.',
+      'Joey doesn\'t share food.',
+      'I love sandwich.'
     ]
   },
     {
     'What did Monica offer Joey when he was moving in next door?': [
-      'Lemonade',
-      'Juice',
-      'Coffee',
-      'Sandwich'
+      'Lemonade.',
+      'Juice.',
+      'Coffee.',
+      'Sandwich.'
     ]
   },
     {
     'Who got a ticket for driving slow?': [
-      'Ross',
-      'Monica',
-      'Joey',
-      'Phoebe'
+      'Ross.',
+      'Monica.',
+      'Joey.',
+      'Phoebe.'
     ]
   },
     {
     'Who turned 30 last?': [
-      'Rachael',
-      'Monica',
-      'Joey',
-      'Chandler'
+      'Rachael.',
+      'Monica.',
+      'Joey.',
+      'Chandler.'
     ]
   },
     {
     'Who cut chandler\'s toe?': [
-      'Monica',
-      'Janice',
-      'Rachel',
-      'Joey'
+      'Monica.',
+      'Janice.',
+      'Rachel.',
+      'Joey.'
     ]
   },
     {
     'What shop did Janic\'s husband had?': [
-      'Matress king',
-      'Ralph Lauren',
-      'Central perk',
-      'Bloomingdale'
+      'Matress king.',
+      'Ralph Lauren.',
+      'Central perk.',
+      'Bloomingdale.'
     ]
   },
     {
     'Who got a Porsche as a gift from their parents?': [
-      'Monica',
-      'Ross',
-      'Chandler',
-      'Rachel'
+      'Monica.',
+      'Ross.',
+      'Chandler.',
+      'Rachel.'
     ]
   },
     {
-    'Who owned a boat in an auction': [
-      'Joey',
-      'Ross',
-      'Rachel',
-      'Janice'
+    'Who owned a boat in an auction?': [
+      'Joey.',
+      'Ross.',
+      'Rachel.',
+      'Janice.'
     ]
   },
     {
     'How many times did Ross get a divorce?': [
-      '3',
-      '4',
-      '2',
-      '1'
+      'Thrice.',
+      '4 times.',
+      'Twice.',
+      'Once.'
     ]
   },
   {
     'What is the giant poking device made out of?': [
-      'Chopsticks',
-      'Plastic spoons',
-      'Pens',
-      'Hangers'
+      'Chopsticks.',
+      'Plastic spoons.',
+      'Pens.',
+      'Hangers.'
     ]
   },
   {
     'What band does Monica, Ross, and Chandler go to see for Ross birthday?': [
-      'Hootie and the Blowfish',
-      'Red Hot Chili Peppers',
-      'Green Day',
-      'Goo Goo Dolls'
+      'Hootie and the Blowfish.',
+      'Red Hot Chili Peppers.',
+      'Green Day.',
+      'Goo Goo Dolls.'
     ]
   },
   {
     'How many sisters does Joey have?': [
-      'Seven',
-      'Eleven',
-      'Eight',
-      'Two'
+      'Seven sisters.',
+      'Eleven sisters.',
+      'Eight sisters.',
+      'Two sisters.'
     ]
   },
   {
-    'What are the 3 Scrabble tiles Marcel swallows': [
-      'M,O, K',
-      'A, K, T',
-      'Q, F, T',
-      'A, I, V'
+    'What are the 3 Scrabble tiles Marcel swallows?': [
+      'M,O, K.',
+      'A, K, T.',
+      'Q, F, T.',
+      'A, I, V.'
     ]
   },
   {
-    'What is the job title of Chandler': [
-      'Statistical Analysis and Data Reconfiguration',
-      'Management Information Systems Director',
-      'Transponster',
-      'Information Technology Coordinator'
+    'What is the job title of Chandler Bing?': [
+      'Statistical Analysis and Data Reconfiguration.',
+      'Management Information Systems Director.',
+      'Transponster.',
+      'Information Technology Coordinator.'
     ]
   },
   {
-    'Who said this: pivot': [
-      'Ross',
-      'Joey',
-      'Rachel',
-      'Chandler'
+    'Who said this: pivot?': [
+      'Ross.',
+      'Joey.',
+      'Rachel.',
+      'Chandler.'
     ]
   },
   {
     'What is Chandler middle name?': [
-      'Muriel',
-      'Maggie',
-      'Moss',
-      'Maurice'
+      'Muriel.',
+      'Maggie.',
+      'Moss.',
+      'Maurice.'
     ]
   },
   {
     'How many categories of towel does Monica have?': [
-      '11',
-      '10',
-      '12',
-      '9'
+      '11 categories.',
+      '10 categories.',
+      '12 categories.',
+      '9 categories.'
     ]
   },
   {
     'What is in Monica\'s locked closet?': [
-      'Junk',
-      'Candy and chocolate',
-      'Skeletons',
-      'Cleaning equipment'
+      'Junk.',
+      'Candy and chocolate.',
+      'Skeletons.',
+      'Cleaning equipment.'
     ]
   }
 ],
     QUESTIONS_EN_US: [
   {
     'What food is Ross allergic to?': [
-      'Lobster',
-      'Chcolate',
-      'Milk',
-      'Ice cream'
+      'Lobster.',
+      'Chcolate.',
+      'Milk.',
+      'Ice cream.'
     ]
   },
     {
     'What according to Phoebe, makes Chandler look homosexual?': [
-      'His Hair',
-      'His Voice',
-      'His Stature',
-      'His Looks'
+      'His Hair.',
+      'His Voice.',
+      'His Stature.',
+      'His Looks.'
     ]
   },
     {
-    'Joey\'s favourite dialogue': [
-      'How you doin',
-      'I love pizza',
-      'Joey doesn\'t share food',
-      'I love sandwich'
+    'Joey\'s favourite dialogue?': [
+      'How you doin.',
+      'I love pizza.',
+      'Joey doesn\'t share food.',
+      'I love sandwich.'
     ]
   },
     {
     'What did Monica offer Joey when he was moving in next door?': [
-      'Lemonade',
-      'Juice',
-      'Coffee',
-      'Sandwich'
+      'Lemonade.',
+      'Juice.',
+      'Coffee.',
+      'Sandwich.'
     ]
   },
     {
     'Who got a ticket for driving slow?': [
-      'Ross',
-      'Monica',
-      'Joey',
-      'Phoebe'
+      'Ross.',
+      'Monica.',
+      'Joey.',
+      'Phoebe.'
     ]
   },
     {
     'Who turned 30 last?': [
-      'Rachael',
-      'Monica',
-      'Joey',
-      'Chandler'
+      'Rachael.',
+      'Monica.',
+      'Joey.',
+      'Chandler.'
     ]
   },
     {
     'Who cut chandler\'s toe?': [
-      'Monica',
-      'Janice',
-      'Rachel',
-      'Joey'
+      'Monica.',
+      'Janice.',
+      'Rachel.',
+      'Joey.'
     ]
   },
     {
     'What shop did Janic\'s husband had?': [
-      'Matress king',
-      'Ralph Lauren',
-      'Central perk',
-      'Bloomingdale'
+      'Matress king.',
+      'Ralph Lauren.',
+      'Central perk.',
+      'Bloomingdale.'
     ]
   },
     {
     'Who got a Porsche as a gift from their parents?': [
-      'Monica',
-      'Ross',
-      'Chandler',
-      'Rachel'
+      'Monica.',
+      'Ross.',
+      'Chandler.',
+      'Rachel.'
     ]
   },
     {
-    'Who owned a boat in an auction': [
-      'Joey',
-      'Ross',
-      'Rachel',
-      'Janice'
+    'Who owned a boat in an auction?': [
+      'Joey.',
+      'Ross.',
+      'Rachel.',
+      'Janice.'
     ]
   },
     {
     'How many times did Ross get a divorce?': [
-      '3',
-      '4',
-      '2',
-      '1'
+      'Thrice.',
+      '4 times.',
+      'Twice.',
+      'Once.'
     ]
   },
   {
     'What is the giant poking device made out of?': [
-      'Chopsticks',
-      'Plastic spoons',
-      'Pens',
-      'Hangers'
+      'Chopsticks.',
+      'Plastic spoons.',
+      'Pens.',
+      'Hangers.'
     ]
   },
   {
     'What band does Monica, Ross, and Chandler go to see for Ross birthday?': [
-      'Hootie and the Blowfish',
-      'Red Hot Chili Peppers',
-      'Green Day',
-      'Goo Goo Dolls'
+      'Hootie and the Blowfish.',
+      'Red Hot Chili Peppers.',
+      'Green Day.',
+      'Goo Goo Dolls.'
     ]
   },
   {
     'How many sisters does Joey have?': [
-      'Seven',
-      'Eleven',
-      'Eight',
-      'Two'
+      'Seven sisters.',
+      'Eleven sisters.',
+      'Eight sisters.',
+      'Two sisters.'
     ]
   },
   {
-    'What are the 3 Scrabble tiles Marcel swallows': [
-      'M,O, K',
-      'A, K, T',
-      'Q, F, T',
-      'A, I, V'
+    'What are the 3 Scrabble tiles Marcel swallows?': [
+      'M,O, K.',
+      'A, K, T.',
+      'Q, F, T.',
+      'A, I, V.'
     ]
   },
   {
-    'What is the job title of Chandler': [
-      'Statistical Analysis and Data Reconfiguration',
-      'Management Information Systems Director',
-      'Transponster',
-      'Information Technology Coordinator'
+    'What is the job title of Chandler Bing?': [
+      'Statistical Analysis and Data Reconfiguration.',
+      'Management Information Systems Director.',
+      'Transponster.',
+      'Information Technology Coordinator.'
     ]
   },
   {
-    'Who said this: pivot': [
-      'Ross',
-      'Joey',
-      'Rachel',
-      'Chandler'
+    'Who said this: pivot?': [
+      'Ross.',
+      'Joey.',
+      'Rachel.',
+      'Chandler.'
     ]
   },
   {
     'What is Chandler middle name?': [
-      'Muriel',
-      'Maggie',
-      'Moss',
-      'Maurice'
+      'Muriel.',
+      'Maggie.',
+      'Moss.',
+      'Maurice.'
     ]
   },
   {
     'How many categories of towel does Monica have?': [
-      '11',
-      '10',
-      '12',
-      '9'
+      '11 categories.',
+      '10 categories.',
+      '12 categories.',
+      '9 categories.'
     ]
   },
   {
     'What is in Monica\'s locked closet?': [
-      'Junk',
-      'Candy and chocolate',
-      'Skeletons',
-      'Cleaning equipment'
+      'Junk.',
+      'Candy and chocolate.',
+      'Skeletons.',
+      'Cleaning equipment.'
     ]
   }
 ],
@@ -451,7 +451,7 @@ module.exports = {
     ]
   },
   {
-    'What is the job title of Chandler': [
+    'What is the job title of Chandler Bing?': [
       'Statistical Analysis and Data Reconfiguration',
       'Management Information Systems Director',
       'Transponster',
